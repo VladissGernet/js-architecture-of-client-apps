5e2259c371e9ded8437a800412fc62d6067cfa7b
VladissGernet <skeimorr@gmail.com>
2026-03-15 10:13:07 +0400

    7.7 Добавит модель для фильтра. WIP

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
+import { render } from './framework/render.js';
 import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
-import { render } from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/tasks-model.js';
+import FilterModel from './model/filter-model.js';
 import { generateFilter } from './mock/filter.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const tasksModel = new TasksModel();
+const filterModel = new FilterModel();
 const boardPresenter = new BoardPresenter({
   boardContainer: siteMainElement,
   tasksModel,
 });
+
 const filters = generateFilter(tasksModel.tasks);
 
 render(new NewTaskButtonView(), siteHeaderElement);
